cc6d15619f51f26828ba72223a2ea2cfb8ce6d64
Saikat Biswas <saikatbiswas1998.info@gmail.com>
2022-04-23 01:49:26 +0600

    print score complete
    Co-Authored-By: Farjana-Mamun <101337117+Farjana-Mamun@users.noreply.github.com>
    Co-Authored-By: naimislam138 <101430088+naimislam138@users.noreply.github.com>

--- Transparent Form/PrintScoreForm.cs
+++ Transparent Form/PrintScoreForm.cs
@@ -30,6 +30,7 @@ namespace Transparent_Form
         private void button_print_Click(object sender, EventArgs e)
         {
             //We need DGVprinter helper for print pdf file
+            //.......
             printer.Title = "Mdemy Student score list";
             printer.SubTitle = string.Format("Date: {0}", DateTime.Now.Date);
             printer.SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip;
