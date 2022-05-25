9b4944bfce8e470e21ab1d35b2856ff316458060
Saikat Biswas <saikatbiswas1998.info@gmail.com>
2022-05-25 15:56:56 +0600

    complete all project

--- Transparent Form/LoginForm.Designer.cs
+++ Transparent Form/LoginForm.Designer.cs
@@ -53,8 +53,9 @@ namespace Transparent_Form
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(950, 56);
+            this.panel1.Size = new System.Drawing.Size(1267, 69);
             this.panel1.TabIndex = 0;
             // 
             // label6
@@ -62,9 +63,10 @@ namespace Transparent_Form
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Transparent;
-            this.label6.Location = new System.Drawing.Point(927, -1);
+            this.label6.Location = new System.Drawing.Point(1236, -1);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(26, 25);
+            this.label6.Size = new System.Drawing.Size(32, 32);
             this.label6.TabIndex = 1;
             this.label6.Text = "X";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -76,18 +78,20 @@ namespace Transparent_Form
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(70, 22);
+            this.label1.Location = new System.Drawing.Point(93, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(255, 25);
+            this.label1.Size = new System.Drawing.Size(326, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "North Western University";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Transparent_Form.Properties.Resources.classroom_50whitepx;
-            this.pictureBox1.Location = new System.Drawing.Point(5, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(7, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(46, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(61, 62);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -97,27 +101,30 @@ namespace Transparent_Form
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
-            this.label2.Location = new System.Drawing.Point(393, 266);
+            this.label2.Location = new System.Drawing.Point(524, 327);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 23);
+            this.label2.Size = new System.Drawing.Size(208, 28);
             this.label2.TabIndex = 1;
             this.label2.Text = "Please Login First";
             // 
             // textBox_password
             // 
             this.textBox_password.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_password.Location = new System.Drawing.Point(313, 408);
+            this.textBox_password.Location = new System.Drawing.Point(417, 502);
+            this.textBox_password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_password.Name = "textBox_password";
-            this.textBox_password.Size = new System.Drawing.Size(325, 27);
+            this.textBox_password.Size = new System.Drawing.Size(432, 32);
             this.textBox_password.TabIndex = 2;
             this.textBox_password.UseSystemPasswordChar = true;
             // 
             // textBox_usrname
             // 
             this.textBox_usrname.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_usrname.Location = new System.Drawing.Point(313, 342);
+            this.textBox_usrname.Location = new System.Drawing.Point(417, 421);
+            this.textBox_usrname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_usrname.Name = "textBox_usrname";
-            this.textBox_usrname.Size = new System.Drawing.Size(325, 27);
+            this.textBox_usrname.Size = new System.Drawing.Size(432, 32);
             this.textBox_usrname.TabIndex = 2;
             // 
             // label3
@@ -125,9 +132,10 @@ namespace Transparent_Form
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
-            this.label3.Location = new System.Drawing.Point(309, 318);
+            this.label3.Location = new System.Drawing.Point(412, 391);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 21);
+            this.label3.Size = new System.Drawing.Size(128, 23);
             this.label3.TabIndex = 1;
             this.label3.Text = "User Name :";
             // 
@@ -136,9 +144,10 @@ namespace Transparent_Form
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
-            this.label4.Location = new System.Drawing.Point(309, 384);
+            this.label4.Location = new System.Drawing.Point(412, 473);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 21);
+            this.label4.Size = new System.Drawing.Size(114, 23);
             this.label4.TabIndex = 1;
             this.label4.Text = "Password :";
             // 
@@ -148,9 +157,10 @@ namespace Transparent_Form
             this.button_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_login.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_login.ForeColor = System.Drawing.Color.White;
-            this.button_login.Location = new System.Drawing.Point(313, 465);
+            this.button_login.Location = new System.Drawing.Point(417, 572);
+            this.button_login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_login.Name = "button_login";
-            this.button_login.Size = new System.Drawing.Size(325, 41);
+            this.button_login.Size = new System.Drawing.Size(433, 50);
             this.button_login.TabIndex = 3;
             this.button_login.Text = "Login";
             this.button_login.UseVisualStyleBackColor = false;
@@ -159,19 +169,20 @@ namespace Transparent_Form
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Transparent_Form.Properties.Resources.North_Western_University_Logo;
-            this.pictureBox2.Location = new System.Drawing.Point(418, 152);
+            this.pictureBox2.Location = new System.Drawing.Point(557, 187);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(115, 103);
+            this.pictureBox2.Size = new System.Drawing.Size(153, 127);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(950, 650);
+            this.ClientSize = new System.Drawing.Size(1267, 800);
             this.Controls.Add(this.button_login);
             this.Controls.Add(this.textBox_usrname);
             this.Controls.Add(this.textBox_password);
@@ -182,6 +193,7 @@ namespace Transparent_Form
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LoginForm";
